435b754b091ea7ac020f1b73b3dae454b2a9b791
TMANPLUS <kbmagoda2024@gmail.com>
2024-10-24 14:45:06 +0200

    Initial commit

---
+++ My_Web/My_Web/urls.py
@@ -0,0 +1,29 @@
+"""
+URL configuration for My_Web project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+from My_Web_Ecom import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include('My_Web_Ecom.urls')),
+    path('',views.index,name='homepage'),
+    path('',views.livestock,name='livestock'),
+    path('',views.vegetables,name='vegetables'),
+    path('',views.product_detail,name='product_detail')
+
+]
